26d47ba57f4e6d29990b578e2bba62ace3fe30ed
Сергей Ложкин <sergei.lozhkin@travelline.ru>
2024-10-07 17:45:15 +0300

    Обновил тесты, добавил новые на проверку очистки и длины

--- Queue/Queue/Queue.cs
+++ Queue/Queue/Queue.cs
@@ -2,32 +2,51 @@ using System.Drawing;
 
 namespace Queue;
 
+// TODO: добавить тест на FIFO
+// TODO: добавить расширение по необходимости
 public class Queue<T>
 {
     private T[] Buffer { get; set; }
-    private int Head { get; set; }
-    private int Tail { get; set; }
+    private int HeadIndex { get; set; }
+    private int TailIndex { get; set; }
     public int Count;
 
     private const uint _defaultLength = 20;
 
-    public Queue()
+    public Queue(uint initialSize = _defaultLength)
     {
         Buffer = new T[_defaultLength];
-        Head = 0;
-        Tail = 0;
+        HeadIndex = 0;
+        TailIndex = 0;
         Count = 0;
     }
 
     public void Enqueue(T element)
     {
-        Buffer[Tail] = element;
-        Tail++;
+        Buffer[TailIndex++] = element;
+        Count++;
+        if (TailIndex > _defaultLength)
+        {
+            TailIndex = 0;
+        }
     }
 
     public T Dequeue()
     {
-        return Buffer[Head++]; 
+        if (Count < 1)
+        {
+            // TODO: Текст исключения на английском
+            throw new IndexOutOfRangeException("Очередь пустая");
+        }
+
+        T element = Buffer[HeadIndex++];
+        Count++;
+        if (HeadIndex > _defaultLength)
+        {
+            HeadIndex = 0;
+        }
+
+        return element;
     }
 
     public bool IsEmpty() => Count == 0; 
@@ -35,8 +54,8 @@ public class Queue<T>
     public void Clear()
     {
         Buffer = [];
-        Head = 0;
-        Tail = 0;
+        HeadIndex = 0;
+        TailIndex = 0;
         Count = 0;
     }
 }
--- Queue/QueueTests/QueueTests.cs
+++ Queue/QueueTests/QueueTests.cs
@@ -5,10 +5,11 @@ using Queue;
 public class QueueTests
 {
     [Fact]
-    public void Enqueue_ShoulBeDequeued()
+    public void Enqueue_As_FIFO()
     {
         Queue<int> queue = new();
         queue.Enqueue(5);
+        queue.Enqueue(10);
 
         Assert.Equal(5, queue.Dequeue());
     }
@@ -28,5 +29,52 @@ public class QueueTests
         queue.Enqueue(5);
 
         Assert.False(queue.IsEmpty());
+    }
+
+    [Fact]
+    public void HasChangedCount()
+    {
+        Queue<int> queue = new();
+        queue.Enqueue(5);
+        queue.Enqueue(6);
+
+        Assert.Equal(2, queue.Count);
+
+        queue.Enqueue(7);
+
+        Assert.Equal(3, queue.Count);
+    }
+
+    [Fact]
+    public void CanBeCleared()
+    {
+        Queue<int> queue = new();
+        queue.Enqueue(5);
+        queue.Enqueue(4);
+        queue.Enqueue(5);
+
+        Assert.Equal(3, queue.Count);
+
+        queue.Clear();
+
+        Assert.Equal(0, queue.Count);
+    }
+
+    [Fact]
+    public void Empty_TryDequeue_ShouldThrowException()
+    {
+        Queue<int> queue = new();
+        
+        bool caughtExpection = false;
+        try
+        {
+            queue.Dequeue();
+        }
+        catch (IndexOutOfRangeException)
+        {
+            caughtExpection = true;
+        }
+
+        Assert.True(caughtExpection);
     }
 }
