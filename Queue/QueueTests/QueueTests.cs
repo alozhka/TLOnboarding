2844c5e5e9c91514b19502b721885a023d1ed5fc
Sergey Lozhkin <sergei.lozhkin@travelline.ru>
2024-10-08 11:27:08 +0300

    Прошёл тест на увеличение очереди

--- Queue/QueueTests/QueueTests.cs
+++ Queue/QueueTests/QueueTests.cs
@@ -3,8 +3,6 @@ namespace QueueTests;
 using Queue;
 
 
-//TODO: добавить и удалять элементы до переполнения очереди (когда begin != 0)
-//TODO: тест на оборачивание кольца без переполнения (количество добавлений больше размеров, но без переполнения)
 public class QueueTests
 {
     [Fact]
@@ -54,29 +52,6 @@ public class QueueTests
     }
 
     [Fact]
-    public void IncreaseCapacityIfOverflows()
-    {
-        Queue<int> queue = new(4);
-
-        queue.Enqueue(5);
-        queue.Enqueue(6);
-        queue.Enqueue(7); // Buffer = [5, 6, 7, 0]
-
-        queue.Dequeue();
-        queue.Dequeue();
-
-        queue.Enqueue(8);
-        queue.Enqueue(9); // Buffer = [9, 6 (старое), 7, 8]
-
-        Assert.Equal(3, queue.Count());
-        int oldCapacity = queue.Capacity;
-        Assert.Equal(4, oldCapacity);
-
-        queue.Enqueue(10); // [9, 10, 7, 8] => [7, 8, 9, 10, 0, 0, 0, 0]
-        Assert.Equal(oldCapacity * 2, queue.Capacity);
-    }
-
-    [Fact]
     public void WorksAsRingBufferWithoutOverflow()
     {
         Queue<int> queue = new(4);
@@ -107,17 +82,40 @@ public class QueueTests
     }
 
     [Fact]
-    public void CanIncreaseCapacity()
+    public void CanIncreaseCapacityIfOverflows()
     {
-        Queue<int> queue = new(2);
+        Queue<int> queue = new(3);
         queue.Enqueue(3);
         queue.Enqueue(4);
 
+        queue.Dequeue();
+        queue.Dequeue();
+
+        queue.Enqueue(5);
+        queue.Enqueue(6);
+        queue.Enqueue(7);
+
+        Assert.Equal(5, queue.Dequeue());
+        Assert.Equal(6, queue.Dequeue());
+        Assert.Equal(7, queue.Dequeue());
+
+        Assert.Equal(0, queue.Count());
+        Assert.True(queue.IsEmpty());
+
+        queue = new Queue<int>(3);
+
+        queue.Enqueue(3);
+        queue.Enqueue(4);
         queue.Enqueue(5);
 
         Assert.Equal(3, queue.Dequeue());
         Assert.Equal(4, queue.Dequeue());
         Assert.Equal(5, queue.Dequeue());
+
+        Assert.Equal(0, queue.Count());
+        Assert.True(queue.IsEmpty());
+
+
     }
 
     [Fact]
