f2102a62d98a0b2669ebd0e142971a7d9aba5288
Sergey Lozhkin <sergei.lozhkin@travelline.ru>
2024-10-08 12:05:08 +0300

    Переписал тесты, перевёл названия в читаемый регистр

--- Queue/QueueTests/QueueTests.cs
+++ Queue/QueueTests/QueueTests.cs
@@ -6,7 +6,7 @@ using Queue;
 public class QueueTests
 {
     [Fact]
-    public void CanUseAsFIFO()
+    public void Can_use_as_FIFO_queue()
     {
         Queue<int> queue = new();
         queue.Enqueue(5);
@@ -14,45 +14,80 @@ public class QueueTests
         queue.Enqueue(20);
         queue.Enqueue(30);
 
-        Assert.False(queue.IsEmpty());
-        Assert.Equal(4, queue.Count());
+        Assert.False(queue.IsEmpty);
+        Assert.Equal(4, queue.Count);
 
         Assert.Equal(5, queue.Dequeue());
-        Assert.False(queue.IsEmpty());
-        Assert.Equal(3, queue.Count());
+        Assert.False(queue.IsEmpty);
+        Assert.Equal(3, queue.Count);
 
         Assert.Equal(10, queue.Dequeue());
-        Assert.False(queue.IsEmpty());
-        Assert.Equal(2, queue.Count());
+        Assert.False(queue.IsEmpty);
+        Assert.Equal(2, queue.Count);
 
         Assert.Equal(20, queue.Dequeue());
-        Assert.False(queue.IsEmpty());
-        Assert.Equal(1, queue.Count());
+        Assert.False(queue.IsEmpty);
+        Assert.Equal(1, queue.Count);
 
         Assert.Equal(30, queue.Dequeue());
-        Assert.True(queue.IsEmpty());
-        Assert.Equal(0, queue.Count());
+        Assert.True(queue.IsEmpty);
+        Assert.Equal(0, queue.Count);
     }
 
     [Fact]
-    public void CanBeCleared()
+    public void Can_be_cleared()
     {
         Queue<int> queue = new();
         queue.Enqueue(5);
         queue.Enqueue(4);
         queue.Enqueue(5);
 
-        Assert.Equal(3, queue.Count());
-        Assert.False(queue.IsEmpty());
+        Assert.Equal(3, queue.Count);
+        Assert.False(queue.IsEmpty);
 
         queue.Clear();
 
-        Assert.Equal(0, queue.Count());
-        Assert.True(queue.IsEmpty());
+        Assert.Equal(0, queue.Count);
+        Assert.True(queue.IsEmpty);
     }
 
     [Fact]
-    public void WorksAsRingBufferWithoutOverflow()
+    public void Capacity_grows_when_size_exceeds_capacity()
+    {
+        Queue<int> queue = new(3);
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+
+        queue.Dequeue();
+        queue.Dequeue();
+
+        queue.Enqueue(5);
+        queue.Enqueue(6);
+        queue.Enqueue(7);
+
+        Assert.Equal(5, queue.Dequeue());
+        Assert.Equal(6, queue.Dequeue());
+        Assert.Equal(7, queue.Dequeue());
+
+        Assert.Equal(0, queue.Count);
+        Assert.True(queue.IsEmpty);
+
+        queue = new Queue<int>(3);
+
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+        queue.Enqueue(5);
+
+        Assert.Equal(3, queue.Dequeue());
+        Assert.Equal(4, queue.Dequeue());
+        Assert.Equal(5, queue.Dequeue());
+
+        Assert.Equal(0, queue.Count);
+        Assert.True(queue.IsEmpty);
+    }
+
+    [Fact]
+    public void Capacity_does_not_grow_until_size_exceeds_capacity()
     {
         Queue<int> queue = new(4);
         queue.Enqueue(5);
@@ -76,67 +111,18 @@ public class QueueTests
         Assert.Equal(9, queue.Dequeue());
         Assert.Equal(10, queue.Dequeue());
 
-        Assert.Equal(1, queue.Count());
-        
+        Assert.Equal(1, queue.Count);
+
         Assert.Equal(4, queue.Capacity);
     }
 
     [Fact]
-    public void CanIncreaseCapacityIfOverflows()
-    {
-        Queue<int> queue = new(3);
-        queue.Enqueue(3);
-        queue.Enqueue(4);
-
-        queue.Dequeue();
-        queue.Dequeue();
-
-        queue.Enqueue(5);
-        queue.Enqueue(6);
-        queue.Enqueue(7);
-
-        Assert.Equal(5, queue.Dequeue());
-        Assert.Equal(6, queue.Dequeue());
-        Assert.Equal(7, queue.Dequeue());
-
-        Assert.Equal(0, queue.Count());
-        Assert.True(queue.IsEmpty());
-
-        queue = new Queue<int>(3);
-
-        queue.Enqueue(3);
-        queue.Enqueue(4);
-        queue.Enqueue(5);
-
-        Assert.Equal(3, queue.Dequeue());
-        Assert.Equal(4, queue.Dequeue());
-        Assert.Equal(5, queue.Dequeue());
-
-        Assert.Equal(0, queue.Count());
-        Assert.True(queue.IsEmpty());
-
-
-    }
-
-    [Fact]
-    public void ThrowsExceptionIfDequeueFromEmptyQueue()
+    public void Cannot_dequeue_from_empty_queue()
     {
         Queue<int> queue = new();
-
         queue.Enqueue(2);
         queue.Dequeue();
 
-
-        bool caughtExpection = false;
-        try
-        {
-            queue.Dequeue();
-        }
-        catch (IndexOutOfRangeException)
-        {
-            caughtExpection = true;
-        }
-
-        Assert.True(caughtExpection);
+        Assert.Throws<IndexOutOfRangeException>(() => queue.Dequeue());
     }
 }
