023c928fb0c474ffcc61a63633f0fa475c30c46b
Sergey Lozhkin <sergei.lozhkin@travelline.ru>
2024-10-08 10:07:33 +0300

    Написал тест с пробегом по кольцу, но не до конца

--- Queue/QueueTests/QueueTests.cs
+++ Queue/QueueTests/QueueTests.cs
@@ -56,7 +56,29 @@ public class QueueTests
     [Fact]
     public void WorksAsRingBufferWithoutOverflow()
     {
+        Queue<int> queue = new(4);
+        queue.Enqueue(5);
+        queue.Enqueue(6);
+        queue.Enqueue(7);
+        queue.Enqueue(8);
 
+        Assert.Equal(4, queue.Capacity);
+
+        Assert.Equal(5, queue.Dequeue());
+        Assert.Equal(6, queue.Dequeue());
+
+        queue.Enqueue(9);
+        queue.Enqueue(10);
+
+        Assert.Equal(7, queue.Dequeue());
+        Assert.Equal(8, queue.Dequeue());
+
+        queue.Enqueue(11);
+        queue.Enqueue(12);
+
+        Assert.Equal(9, queue.Dequeue());
+        Assert.Equal(10, queue.Dequeue());
+        Assert.Equal(2, queue.Capacity);
     }
 
     [Fact]
