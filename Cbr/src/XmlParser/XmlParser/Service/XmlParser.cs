c329707cadf148e06ee4d70d16e57f911e188b4b
Sergey <100339481+alozhka@users.noreply.github.com>
2024-10-09 20:45:48 +0300

    Расписал абстракцию у классов

---
+++ Cbr/src/XmlParser/XmlParser/Service/XmlParser.cs
@@ -0,0 +1,14 @@
+using XmlParser.Model;
+
+namespace XmlParser.Service;
+
+public class XmlParser
+{
+    public static XmlElement FromFile(string filepath)
+    {
+    }
+    
+    public static XmlElement FromRawString(string rawXml)
+    {
+    }
+}
