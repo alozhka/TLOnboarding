93a08ebf2f8e2e847b244445bfe40976732c7e91
Sergey <100339481+alozhka@users.noreply.github.com>
2024-10-09 21:13:16 +0300

    Сделал общий метод для парсинга, его вызывают все остальные

--- Cbr/src/XmlParser/XmlParser/Service/XmlParser.cs
+++ Cbr/src/XmlParser/XmlParser/Service/XmlParser.cs
@@ -2,13 +2,30 @@ using XmlParser.Model;
 
 namespace XmlParser.Service;
 
-public class XmlParser
+public static class XmlParser
 {
     public static XmlElement FromFile(string filepath)
     {
+        Stream stream = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+        return Parse(stream);
     }
     
     public static XmlElement FromRawString(string rawXml)
     {
+        return Parse(rawXml.ToStream());
+    }
+
+    private static XmlElement Parse(Stream stream)
+    {
+    }
+
+    private static Stream ToStream(this string str)
+    {
+        MemoryStream stream = new();
+        StreamWriter writer = new(stream);
+        writer.Write(str);
+        stream.Flush();
+        stream.Position = 0;
+        return stream;
     }
 }
