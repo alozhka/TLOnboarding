d27632c6f27275297570a11325f4ddbadf3ff409
Sergey <100339481+alozhka@users.noreply.github.com>
2024-10-09 20:09:20 +0300

    Создал шаблоны для проекта, расписал тесты

---
+++ Cbr/src/XmlParser/XmlParser.Tests/XmlParserTests.cs
@@ -0,0 +1,21 @@
+using Xunit;
+
+namespace XmlParser.Tests;
+
+public class XmlParserTests
+{
+    [Fact]
+    public void Can_use_complex_types()
+    {
+    }
+
+    [Fact]
+    public void Supports_parsing_raw_data_and_from_file()
+    {
+    }
+
+    [Fact]
+    public void Cannot_parse_incorrect_xml()
+    {
+    }
+}
