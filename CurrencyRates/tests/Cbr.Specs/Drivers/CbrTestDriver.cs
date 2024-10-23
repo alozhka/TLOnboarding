179298c127d912ddba4734071ea44fefd70f0ff8
Sergey Lozhkin <sergei.lozhkin@travelline.ru>
2024-10-23 13:11:52 +0300

    Добавил получение курсов по дате в драйвер

--- CurrencyRates/tests/Cbr.Specs/Drivers/CbrTestDriver.cs
+++ CurrencyRates/tests/Cbr.Specs/Drivers/CbrTestDriver.cs
@@ -1,6 +1,22 @@
+using System.Text.Json;
+using Cbr.Application.Dto;
+
 namespace Cbr.Specs.Drivers;
 
-public class CbrTestDriver
+public class CbrTestDriver(HttpClient httpClient)
 {
+    public async Task<CbrDayRatesDto> GetDayRates(DateOnly? requestDate = null)
+    {
+        string query = "api/v1/cbr/daily-rates";
+        if (requestDate != null)
+        {
+            query += "/requestDate=" + requestDate?.ToString("yyyy-MM-dd");
+        }
+        
+        HttpResponseMessage response = await httpClient.GetAsync(query);
 
+        string content = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<CbrDayRatesDto>(content) 
+            ?? throw new ArgumentException($"Unexpected JSON response: {content}");
+    }
 }
